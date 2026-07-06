3c66dc1acd94bbf5870f335282f23a267b87e6cc
DanielSousa07 <josenidaniel88@gmail.com>
2026-07-06 11:13:46 -0300

    pfv

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,10 @@
 /// <reference types="vite/client" />
 
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_IFMA_MODE: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
